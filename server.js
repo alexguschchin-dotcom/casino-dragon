3fb1229c080876044e8af5ee1473c3233a490210
alexguschchin-dotcom <alexguschchin@gmail.com>
2026-04-26 18:35:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Раздаём статические файлы из папки public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Все остальные запросы направляем на index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Сервер квеста "Сага о проклятом рыцаре" запущен на порту ${PORT}`);
-});
+app.use(express.static(__dirname));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
