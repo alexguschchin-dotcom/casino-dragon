81b159c381a03e96bedd85d81ca942d276e2c6f2
alexguschchin-dotcom <alexguschchin@gmail.com>
2026-06-09 23:16:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Статика из папки public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Все остальные запросы -> index.html из public
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
+    console.log(`Летняя викторина запущена на порту ${PORT}`);
 });
